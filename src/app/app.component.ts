c89913a2e094b94ce8871a7d2a246a840cf8394d
Cesar Zaragoza <zaragoza0711@gmail.com>
2025-01-26 18:30:37 -0600

    finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,61 +32,63 @@ information6: string = "";
 
 ngOnInit(): void {
 //llamar a metodo carrusel
-this.carrusel(1);
+  this.carrusel(1);
 }
+
 carrusel(op: number): void {
-new Splide(".mycarousel", {
-perPage: 3,
-perMove: 1,
-padding: "5%",
-rewind: true,
-}).mount();
+  new Splide(".mycarousel", 
+    {
+      perPage: 3,
+      perMove: 1,
+      padding: "5%",
+      rewind: true,
+    }).mount();
 
-switch (op) {
-case 1:
-this.img1 = "/mexicana.jpg";
-this.information1 = "Pizza Mexicana <br> $230.00";
-this.img2 = "/romana.jpg";
-this.information2 = "Pizza Romana <br> $180.00";
-this.img3 = "/hawayana.jpg";
-this.information3 = "Pizza Hawayana <br> $120.00";
-this.img4 = "/peperoni.jpg";
-this.information4 = "Pizza Peperoni <br> $190.00";
-this.img5 = "/queso.jpg";
-this.information5 = "Pizza Queso <br> $100.00";
-this.img6 = "/atun.jpg";
-this.information6 = "Pizza Atun <br> 125.00";
-break;
+  switch (op) {
+    case 1:
+      this.img1 = "/mexicana.jpg";
+      this.information1 = "Pizza Mexicana <br> $230.00";
+      this.img2 = "/romana.jpg";
+      this.information2 = "Pizza Romana <br> $180.00";
+      this.img3 = "/hawayana.jpg";
+      this.information3 = "Pizza Hawayana <br> $120.00";
+      this.img4 = "/peperoni.jpg";
+      this.information4 = "Pizza Peperoni <br> $190.00";
+      this.img5 = "/queso.jpg";
+      this.information5 = "Pizza Queso <br> $100.00";
+      this.img6 = "/atun.jpg";
+      this.information6 = "Pizza Atun <br> 125.00";
+    break;
 
-case 2:
-this.img1 = "/polloking.jpg";
-this.information1 = "Hamburguesa de pollo <br> $90.00";
-this.img2 = "/baconking.jpg";
-this.information2 = "Hamburguesa bacon <br> $95.00";
-this.img3 = "/clasicaking.jpg";
-this.information3 = "Hamburguesa clasica <br> $60.00";
-this.img4 = "/doblecarneking.jpg";
-this.information4 = "Hamburguesa doble carne <br> $130.00";
-this.img5 = "/quesoking.jpg";
-this.information5 = "Hamburguesa de queso <br> $50.00";
-this.img6 = "/whopperking.jpg";
-this.information6 = "Hamburguesa whopper <br> $160.00";
-break;
+    case 2:
+      this.img1 = "/polloking.jpg";
+      this.information1 = "Hamburguesa de pollo <br> $90.00";
+      this.img2 = "/baconking.jpg";
+      this.information2 = "Hamburguesa bacon <br> $95.00";
+      this.img3 = "/clasicaking.jpg";
+      this.information3 = "Hamburguesa clasica <br> $60.00";
+      this.img4 = "/doblecarneking.jpg";
+      this.information4 = "Hamburguesa doble carne <br> $130.00";
+      this.img5 = "/quesoking.jpg";
+      this.information5 = "Hamburguesa de queso <br> $50.00";
+      this.img6 = "/whopperking.jpg";
+      this.information6 = "Hamburguesa whopper <br> $160.00";
+    break;
 
-case 3:
-this.img1 = "/caramelo.jpg";
-this.information1 = "capuchino caramelo <br> $85.00";
-this.img2 = "/clasico.jpg";
-this.information2 = "cafe clasico <br> $55.00";
-this.img3 = "/moka.jpg";
-this.information3 = "cafe Moka <br> $80.00";
-this.img4 = "/unicornio.jpg";
-this.information4 = "cafe unicornio <br> $90.00";
-this.img5 = "/cafehelado.jpg";
-this.information5 = "capuchino caramelo <br> $60.00";
-this.img6 = "/cafefrio.jpg";
-this.information6 = "capuchino caramelo <br> $65.00";
-break;
-}//fin swith
-}//fin del metodo carrusel
+    case 3:
+      this.img1 = "/caramelo.jpg";
+      this.information1 = "capuchino caramelo <br> $85.00";
+      this.img2 = "/clasico.jpg";
+      this.information2 = "cafe clasico <br> $55.00";
+      this.img3 = "/moka.jpg";
+      this.information3 = "cafe Moka <br> $80.00";
+      this.img4 = "/unicornio.jpg";
+      this.information4 = "cafe unicornio <br> $90.00";
+      this.img5 = "/cafehelado.jpg";
+      this.information5 = "capuchino caramelo <br> $60.00";
+      this.img6 = "/cafefrio.jpg";
+      this.information6 = "capuchino caramelo <br> $65.00";
+    break;
+    }//fin swith
+  }//fin del metodo carrusel
 }//fin de la clase
